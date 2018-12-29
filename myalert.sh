1ca22eaf0fa985ab4838dc36383c8807e967af29
sot <swtevag@gmail.com>
2018-12-29 14:20:48 +0200

    Supports .wav files

--- myalert.sh
+++ myalert.sh
@@ -1,7 +1,13 @@
 for file in *.mp3
 	do ffmpeg -i "${file}" "${file/%mp3/ogg}"
 done
-mkdir -p "my_ogg_bells"
+
+for file in *.wav
+	do ffmpeg -i "${file}" -acodec libvorbis "${file/%wav/ogg}"
+done
+
+mkdir -p "my_ogg_bells"  # bells in ubuntu are all .ogg sound files.
+
 for song in *.ogg
 	do mv "${song}" "my_ogg_bells/."
 done
